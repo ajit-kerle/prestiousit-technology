a3f1519708266145db9fa0f0ea6b2a284dee9efe
ajit kerle <ajitkerle70@gmail.com>
2023-01-06 14:10:35 +0530

    build in pregress

--- client/src/components/Headtail.js
+++ client/src/components/Headtail.js
@@ -1,46 +1,46 @@
-import react,{ useEffect, useState } from "react";
-
+import react, { useEffect, useState } from "react";
 
 function Headtail() {
-  const [value,setValue]=useState("H")
-  const handleChange=(event) =>{
-    setValue({value: event.target.value});
+  const [seletedOption, setSeletedOption] = useState();
+  // const [result, setResult] = useState([
+  //   ["H", "T"],
+  //   [" ", "T"]
+  // ]);
+  const [result, setResult] = useState([]);
+  function handleChange(event) {
+    setSeletedOption(event.target.value);
   }
-  
-  const handleSubmit=(event)=>{
-      setValue({value: event.target.value});
-  }
-//   useEffect(())
-    
+
+  const handleClick = () => {
+    setResult(() => [...result, { val: seletedOption }]);
+  };
+
+  // [[H,T],[' ', T]]
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: "20px",
-        }}
-      >
-        <h1>Head and tail page</h1>
-         <form onSubmit={handleSubmit}>
-        <label>
-          <select value={value} onChange={handleChange}>
-            {/* <option selected value="select_something">select something</option> */}
-            <option value="T">T</option>
-            <option value="H">H</option>
-            
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-       <table>
-        <thead>
-            <tr>
-                <th>{value}</th>
-            </tr>
-        </thead>
-       </table>
+      <div className="App">
+        
+        <h2>Head and tail page</h2>
+
+        <select onChange={(e) => handleChange(e)}>
+          <option value="H">H </option>
+          <option value="T">T </option>
+        </select>
+
+        <button onClick={handleClick}> submit </button>
+
+        {result.map((obj, ind) => (
+          <p key={ind}>{obj.val ? <h1>{obj.val} </h1> : <h6>{obj.val}</h6>}</p>
+        ))}
+
+        {/* {result.map((arr) =>(
+        arr.map((elem, ind) => (
+          <p style={{ display: "flex" }} key={ind}>
+            {" "}
+            {elem}{" "}
+          </p>
+        ))
+      ))} */}
       </div>
     </>
   );
